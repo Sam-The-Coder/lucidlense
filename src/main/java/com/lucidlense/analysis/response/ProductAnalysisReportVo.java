9b5b58bac7689f561f51c298b085744f968db8dc
Sneha <30997434+Sam-The-Coder@users.noreply.github.com>
2020-11-04 18:03:39 +0530

    Updating ProductAnalysisReportVo
    Updating ProductAnalysisReportVo

--- src/main/java/com/lucidlense/analysis/response/ProductAnalysisReportVo.java
+++ src/main/java/com/lucidlense/analysis/response/ProductAnalysisReportVo.java
@@ -1,11 +1,13 @@
 package com.lucidlense.analysis.response;
 
 
+import com.lucidlense.analysis.request.Feature;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
 
 import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
 
 @Component
 @Getter
@@ -13,4 +15,32 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public class ProductAnalysisReportVo
 {
+    private String productName;
+    private String description;
+    private String vision;
+    private String developmentCost;
+    private String maintenanceCosts;
+    private String licencingCost;
+    private List<Feature> features;
+    private String uiFriendliness;
+    private String innovation;
+    private String securityRating;
+    private String domain;
+    private String usability;
+    private String interoperability;
+    private String integration;
+    private String technologyStack;
+    private String revenue;
+    private String customerSentimentRating;
+    private String operationalRegion;
+    private String customers;
+    private String legalCompliance;
+    private String security;
+    private String socialImpact;
+    private String supportService;
+    private String reliability;
+    private String availability;
+    private String scalability;
+    private String licenseType;
+
 }
